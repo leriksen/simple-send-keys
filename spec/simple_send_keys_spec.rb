fbb530a3682870111986165b558961ab9e2f906c
leif.eriksen <leif.eriksen@cgu.com.au>
2015-07-13 11:27:42 +1000

    coverage test

--- spec/simple_send_keys_spec.rb
+++ spec/simple_send_keys_spec.rb
@@ -2,10 +2,19 @@ require 'spec_helper'
 require 'simple_send_keys'
 
 describe 'SimpleSendKeys' do
-  let(:native) {double('native')}
-  it 'can send keys' do
+  before(:each) do
     self.extend SimpleSendKeys
+  end
+
+  let(:native) {double('native')}
+
+  it 'can send keys in a list format' do
     expect(native).to receive(:send_keys).once.with([:backspace, 'gist', :f1, '123'])
     send_keys('[backspace, "gist", f1 , 123]')
+  end
+
+  it 'can send keys in a string format' do
+    expect(native).to receive(:send_keys).once.with(['gist'])
+    send_keys('gist')
   end
 end
